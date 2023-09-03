099df82c8cee967878def7836e9400af308366f8
Gumerov-Vadim <95773619+Gumerov-Vadim@users.noreply.github.com>
2023-09-03 11:19:17 +0500

    Added comment input

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,29 @@
     <div class="comment_header">
         Оставьте комментарий!
     </div>
-    <input type="text">
-    <div class="send">
-        <div class="send_text">Отправить!</div>
-        <div class="send_icon"></div>
+    <textarea v-bind:value="Content" @input="inputContent" name="comment_field" id="comment_field" cols="30" rows="10" placeholder="Напишите, что вы думаете..." ></textarea>
+    <div class="comment_footer">
+        <div class="send_button">
+            <div class="send_text">Отправить!</div>
+            <div class="send_icon"></div>
+        </div>
     </div>
 </div>   
 </template>
     
     <script>
+    import Comment from "@/components/Comment.vue"
+    import CommentInput from "@/components/Comment.vue"
     export default{
         data(){
             return{
+                Content:"123"
+            }
+        },
+        methods:{
+            inputContent(event){
+                console.log(event);
+                this.Content = event.target.value;
             }
         }
     }
@@ -22,21 +33,44 @@
     
     <style scoped>
     .comment_input{
+        color: white;
         display: flex;
         flex-direction: column;
         width: 600px;
         padding: 10px;
-        background-color: #8DDCB8;
+        background-color: #41B882;
     }
-    input{
-        background-color: #8DDCB8;
-    }
-    .send{
-        color:#71DCAB;
+    textarea{
+        outline : none;
+        resize: none;
         background-color: #15784B;
+        min-height: 100px;
+        max-height: 300px;
+        color: white;
+    }
+    textarea::placeholder{
+        color: #41B882;
+    }
+    .comment_footer{
         display: flex;
         flex-direction: row;
         justify-content: flex-end;
+        align-items: flex-end;
+    }
+    .send_button{
+        margin: 2px 0 0 2px;
+        padding: 2px;
+        border-radius: 5px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+        background-color: #478A6C;
+        color: #41B882;
+        cursor: pointer;
+    }
+    .send_button:hover{
+        color:#98E752 ;
+        box-shadow:0 0 0 1px #98E752 ;
     }
     .send_icon{
         height: 26px;
---
+++ src/components/CommentInput.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="comment_input">
+        <div class="comment_header">
+            Оставьте комментарий!
+        </div>
+        <textarea v-bind:value="Content" @input="inputContent" name="comment_field" id="comment_field" cols="30" rows="10" placeholder="Напишите, что вы думаете..." ></textarea>
+        <div class="comment_footer">
+            <div class="send_button">
+                <div class="send_text">Отправить!</div>
+                <div class="send_icon"></div>
+            </div>
+        </div>
+    </div>   
+    </template>
+        
+        <script>
+        export default{
+            data(){
+                return{
+                    Content:"123"
+                }
+            },
+            methods:{
+                inputContent(event){
+                    this.Content = event.target.value;
+                }
+            }
+        }
+        </script>
+        
+        <style scoped>
+        .comment_input{
+            color: white;
+            display: flex;
+            flex-direction: column;
+            width: 600px;
+            padding: 10px;
+            background-color: #41B882;
+        }
+        textarea{
+            outline : none;
+            resize: none;
+            background-color: #15784B;
+            min-height: 100px;
+            max-height: 300px;
+            color: white;
+        }
+        textarea::placeholder{
+            color: #41B882;
+        }
+        .comment_footer{
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-end;
+            align-items: flex-end;
+        }
+        .send_button{
+            margin: 2px 0 0 2px;
+            padding: 2px;
+            border-radius: 5px;
+            display: flex;
+            justify-content: flex-end;
+            align-items: flex-end;
+            background-color: #478A6C;
+            color: #41B882;
+            cursor: pointer;
+        }
+        .send_button:hover{
+            color:#98E752 ;
+            box-shadow:0 0 0 1px #98E752 ;
+        }
+        .send_icon{
+            height: 26px;
+            width: 26px;
+            background-size: cover;
+            background-image: url(@/img/send.png);
+        }
+        *{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        </style>
